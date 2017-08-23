ca86c02e5443e5a0444360202a1b34048bc07510
yurastepanyuk <yu.stepanyu.ra@gmail.com>
2017-08-23 17:21:36 +0300

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -39,6 +39,7 @@ import { OfflineConsultationComponent } from './offline-consultation/offline-con
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
