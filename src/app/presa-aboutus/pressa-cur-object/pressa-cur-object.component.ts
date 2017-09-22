cc8adba9441f7eb82e731103a5ee7604a6e79077
yurastepanyuk <yu.stepanyu.ra@gmail.com>
2017-09-22 20:19:43 +0300

    view list from http servise /presa-abou-us/

--- src/app/presa-aboutus/pressa-cur-object/pressa-cur-object.component.ts
+++ src/app/presa-aboutus/pressa-cur-object/pressa-cur-object.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {PressaAboutUs} from '../../dtd/pressa-about-us.model';
+import {AbstractControl, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-pressa-cur-object',
@@ -8,15 +9,49 @@ import {PressaAboutUs} from '../../dtd/pressa-about-us.model';
 })
 export class PressaCurObjectComponent implements OnInit {
 
-  @Input() pressaOnject: PressaAboutUs;
+  _pressaObject: PressaAboutUs;
+
+  @Input()
+  set pressaObject(pressaObject: PressaAboutUs) {
+    // console.log('prev value: ', this._pressaObject);
+    // console.log('got name: ', pressaObject);
+    this._pressaObject = pressaObject;
+    this.initObject();
+  }
 
   isThereMedia: boolean;
   isThereDownload: boolean;
   isThereFolder: boolean;
 
-  constructor() { }
+  myForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+
+    this.myForm = fb.group({
+      'pressaObject':  [{}]
+    });
+
+  }
 
   ngOnInit() {
   }
+  initObject(): void {
+    console.log('initObject: ', this.pressaObject);
+
+
+
+  }
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   const name: SimpleChange = changes.name;
+  //   console.log('prev value: ', name.previousValue);
+  //   console.log('got name: ', name.currentValue);
+  //   this._name = name.currentValue.toUpperCase();
+  // }
+
+  // get name(): string {
+  //   // transform value for display
+  //   return this._name.toUpperCase();
+  // }
 
 }
--- src/app/presa-aboutus/pressa-service.service.ts
+++ src/app/presa-aboutus/pressa-service.service.ts
@@ -4,6 +4,9 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import {PressaAboutUs} from '../dtd/pressa-about-us.model';
 
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
 
 @Injectable()
 export class PressaServiceService {
@@ -15,7 +18,7 @@ export class PressaServiceService {
  getData(): Observable<PressaAboutUs[]>  {
    const people$ = this.http.get('api/presa-aboutus').map((response: Response) => {
      return this.mapPersons(response);
-   } );
+   } ).catch(this.handleError);
 
    return people$;
 
@@ -52,6 +55,52 @@ export class PressaServiceService {
     console.log('Parsed person:', person);
     return person;
   }
+
+  // this could also be a private method of the component class
+  handleError (error: any) {
+  // log error
+  // could be something more sofisticated
+  const errorMsg = error.message || `There was a problem with our hyperdrive device and we couldn't retrieve your data!`
+  console.error(errorMsg);
+
+  // throw an application level error
+  return Observable.throw(errorMsg);
+}
+
+//   getAll(): Observable<PressaAboutUs[]>{
+//     let people$ = this.http
+//       .get(`${this.baseUrl}/people`, { headers: this.getHeaders()})
+//       .map(mapPersons)
+//       .catch(handleError);
+//     return people$;
+//   }
+//
+//   private getHeaders(){
+//     // I included these headers because otherwise FireFox
+//     // will request text/html
+//     let headers = new Headers();
+//     headers.append('Accept', 'application/json');
+//     return headers;
+//   }
+//   get(id: number): Observable<PressaAboutUs> {
+//     let person$ = this.http
+//       .get(`${this.baseUrl}/people/${id}`, {headers: this.getHeaders()})
+//       .map(mapPerson)
+//       .catch(handleError);
+//     return person$;
+//   }
+//
+//   save(person: PressaAboutUs) : Observable<Response>{
+//      // this won't actually work because the StarWars API doesn't
+//     // is read-only. But it would look like this:
+//     return this
+//       .http
+//       .put(`${this.baseUrl}/people/${person.id}`,
+//         JSON.stringify(person),
+//         {headers: this.getHeaders()});
+//   }
+//
+// }
 
 
 }
